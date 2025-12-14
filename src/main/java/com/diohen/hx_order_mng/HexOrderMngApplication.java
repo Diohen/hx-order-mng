9141514c547f5409208ed00cd72c8e447d01ab08
Diohen <diogohss17@gmail.com>
2025-12-14 17:24:45 -0300

    Hex Order Mng Api

---
+++ src/main/java/com/diohen/hx_order_mng/HexOrderMngApplication.java
@@ -0,0 +1,13 @@
+package com.diohen.hx_order_mng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexOrderMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexOrderMngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diohen/hx_order_mng/HexOrderMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diohen.hx_order_mng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexOrderMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
